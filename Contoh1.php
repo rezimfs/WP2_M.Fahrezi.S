d45d269850d0d8b11c412bad0c5f913cf4e6f072
rezimfs <90608443+rezimfs@users.noreply.github.com>
2021-10-02 11:26:30 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo "Nama Saya Muhammad Fahrezi Syahputra \n Saya tinggal di daerah Jakarta Utara \n Hobby saya adalah bermain game."
+	}
+}
